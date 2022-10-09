b3fd82f7f463047c272403c709888f85e5b0d564
Autumn <autumn@inflab.com>
2022-10-09 20:42:30 +0900

    [12-1] 메서드 올리기

--- ch12/12-1.js
+++ ch12/12-1.js
@@ -1,24 +1,26 @@
+/**
+ * BAD:
+ * - 자식 클래스들에서 동일한 메서드를 정의하고 있다.
+ *
+ * SOLUTION:
+ * - 공통된 메서드는 부모 클래스로 올린다.
+ * */
+
 // 예시 1
-class Employee {}
-
-class Salesperson extends Employee {
+class Employee {
   get name() {}
 }
 
-class Engineer extends Employee {
-  get name() {}
-}
+class Salesperson extends Employee {}
+
+class Engineer extends Employee {}
 
 // 예시 2
-class Party {}
-
-class Department extends Party {
-  get totalAnnualCost() {
-    return this.monthlyCost * 12;
-  }
-}
-class Employee extends Party {
+class Party {
   get annualCost() {
     return this.monthlyCost * 12;
   }
 }
+
+class Department extends Party {}
+class Employee extends Party {}
