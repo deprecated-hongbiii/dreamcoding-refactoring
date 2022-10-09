add8e031c4ccce305bc80eff4cf690bfe155b4b3
Autumn <autumn@inflab.com>
2022-10-09 21:01:24 +0900

    [12-4] 생성자 본문 올리기

--- ch12/12-3.js
+++ ch12/12-3.js
@@ -1,23 +1,32 @@
-class Party {}
+/**
+ * BAD:
+ * - 자식클래스에서 중복된 필드, 생성자 로직
+ *
+ * SOLUTION:
+ * - 중복되는 #name을 부모클래스로 올리고 super의 매개변수로 넘겨준다.
+ * */
+
+class Party {
+  #name;
+  constructor(name) {
+    this.#name = name;
+  }
+}
 
 class Employee extends Party {
-  #name;
   #id;
   #monthlyCost;
   constructor(name, id, monthlyCost) {
-    super();
-    this.#name = name;
+    super(name);
     this.#id = id;
     this.#monthlyCost = monthlyCost;
   }
 }
 
 class Department extends Party {
-  #name;
   #staff;
   constructor(name, staff) {
-    super();
-    this.#name = name;
+    super(name);
     this.#staff = staff;
   }
 }
