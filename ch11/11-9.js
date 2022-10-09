96cd3fd327d8c8b142731b8e4ebf94e0d6dba16c
Autumn <autumn@inflab.com>
2022-10-09 19:10:30 +0900

    [11-9] 함수를 명령으로 바꾸기

--- ch11/11-9.js
+++ ch11/11-9.js
@@ -1,20 +1,38 @@
+/**
+ * POINT:
+ * - 절차지향적 코드를 객체지향적으로 리팩토링하기
+ * - 명령을 수행하는 함수를 딱 하나 가지고 있는 객체를 "명령 객체 (command)" 라고 한다.
+ *  - do, undo를 묶어 만들 수 있는 것이 장점 (??? 왜 장점인지 이해가 안 됨)
+ * */
 export function score(candidate, medicalExam, scoringGuide) {
-  let result = 0;
-  let healthLevel = 0;
-  let highMedicalRiskFlag = false;
+  return new Scorer(candidate, medicalExam, scoringGuide).execute();
+}
 
-  if (medicalExam.isSmoker) {
-    healthLevel += 10;
-    highMedicalRiskFlag = true;
+class Scorer {
+  constructor(candidate, medicalExam, scoringGuide) {
+    this.candidate = candidate;
+    this.medicalExam = medicalExam;
+    this.scoringGuide = scoringGuide;
   }
-  let certificationGrade = 'regular';
-  if (scoringGuide.stateWithLowCertification(candidate.originState)) {
-    certificationGrade = 'low';
-    result -= 5;
+
+  execute() {
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+
+    if (this.medicalExam.isSmoker) {
+      healthLevel += 10;
+      highMedicalRiskFlag = true;
+    }
+    let certificationGrade = 'regular';
+    if (this.scoringGuide.stateWithLowCertification(this.candidate.originState)) {
+      certificationGrade = 'low';
+      result -= 5;
+    }
+    // lots more code like this
+    result -= Math.max(healthLevel - 5, 0);
+    return result;
   }
-  // lots more code like this
-  result -= Math.max(healthLevel - 5, 0);
-  return result;
 }
 
 export class ScoringGuide {
