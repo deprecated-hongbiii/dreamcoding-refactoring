f5dc7f2ca8654d44b592180c9da74e2fd127e91d
Autumn <autumn@inflab.com>
2022-10-09 18:34:08 +0900

    [11-8] 생성자를 팩터리 함수로 바꾸기

--- ch11/11-8.js
+++ ch11/11-8.js
@@ -1,5 +1,15 @@
+/**
+ * POINT:
+ * - 생성자를 캡슐화하기
+ * - 외부에서는 Factory 함수를 통해 간편하게 인스턴스를 생성
+ *
+ * SOLUTION
+ * - constructor는 private으로 만든다.
+ * - 팩토리 메서드는 여러 개 만들 수 있다.
+ *  - typeCode로 무엇을 주어야 할 지 고민하지 않고도 쉽게 인스턴스를 생성할 수 있다.
+ * */
 export class Employee {
-  constructor(name, typeCode) {
+  constructor(name, typeCode) { // TS에서는 private constructor 가능
     this._name = name;
     this._typeCode = typeCode;
   }
@@ -14,4 +24,19 @@ export class Employee {
   static get legalTypeCodes() {
     return { E: 'Engineer', M: 'Manager', S: 'Salesman' };
   }
+
+  static createEngineer(name) {
+    return new Employee(name, 'E')
+  }
+
+  static createSeniorEngineer(name) {
+    return new Employee(name, 'SE');
+  }
+
+  static createMarketer(name) {
+    return new Employee(name, 'M')
+  }
 }
+
+const employee = new Employee('엘리', 'E'); // 💩 BAD: 엔지니어로 만들고 싶은데 타입코드 뭐지..?
+const employee2 = Employee.createEngineer('엘리'); // ✨ GOOD
