a9419b522e41eed44beba21bbc2994662b9dfbb1
Autumn <autumn@inflab.com>
2022-10-09 12:07:49 +0900

    [11-2] 함수 매개변수화하기

--- ch11/11-2.js
+++ ch11/11-2.js
@@ -1,30 +1,37 @@
 // 예제 1
-function tenPercentRaise(person) {
-  person.salary = person.salary.multiply(1.1);
-}
-
-function fivePercentRaise(person) {
-  person.salary = person.salary.multiply(1.05);
+/**
+ * BAD:
+ * - 동일한 로직이 반복된다.
+ *
+ * SOLUTION:
+ * - '얼마만큼'을 매개변수로 받는다.
+ * */
+function raise(person, factor) {
+  person.salary = person.salary.multiply(1 + factor);
 }
 
 // 예제 2
+/**
+ * BAD:
+ * - Band가 반복되고 있다.
+ *
+ * SOLUTION:
+ * - 범위를 매개변수로 받아 해결한다.
+ *
+ * TIPS:
+ * - 이때 unit test가 존재한다면 안전하게 리팩토링할 수 있다.
+ * */
 export function baseCharge(usage) {
   if (usage < 0) return usd(0);
   const amount =
-    bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+    withinBand(usage, 0, 100) * 0.03 +
+    withinBand(usage, 100, 200) * 0.05 +
+    withinBand(usage, 200, Infinity) * 0.07;
   return usd(amount);
 }
 
-function bottomBand(usage) {
-  return Math.min(usage, 100);
-}
-
-function middleBand(usage) {
-  return usage > 100 ? Math.min(usage, 200) - 100 : 0;
-}
-
-function topBand(usage) {
-  return usage > 200 ? usage - 200 : 0;
+function withinBand(usage, bottom, top) {
+  return usage > bottom ? Math.min(usage, top) - bottom : 0;
 }
 
 function usd(value) {
