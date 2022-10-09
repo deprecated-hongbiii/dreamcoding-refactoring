abd2d8bf8932563c1b60f396029b1d064e8d7634
Autumn <autumn@inflab.com>
2022-10-09 20:45:54 +0900

    [12-2] 필드 올리기

--- ch12/12-2.js
+++ ch12/12-2.js
@@ -1,9 +1,14 @@
-class Employee {}
-
-class Salesperson extends Employee {
-  #name;
+/**
+ * BAD:
+ * - 자식클래스들에 공통된 필드(#name)가 있다.
+ *
+ * SOLUTION:
+ * - 공통된 필드를 부모클래스로 올린다.
+ * */
+class Employee {
+  #name
 }
 
-class Engineer extends Employee {
-  #name;
-}
+class Salesperson extends Employee {}
+
+class Engineer extends Employee {}
