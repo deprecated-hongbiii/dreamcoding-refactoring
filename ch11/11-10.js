c74ebdc9359aba3db1a5cd32c5ffa0e88f4d2c8c
Autumn <autumn@inflab.com>
2022-10-09 19:17:20 +0900

    [11-10] 명령을 함수로 바꾸기

--- ch11/11-10.js
+++ ch11/11-10.js
@@ -1,13 +1,15 @@
-export class ChargeCalculator {
-  constructor(customer, usage, provider) {
-    this._customer = customer;
-    this._usage = usage;
-    this._provider = provider;
-  }
-  get baseCharge() {
-    return this._customer.baseRate * this._usage;
-  }
-  get charge() {
-    return this.baseCharge + this._provider.connectionCharge;
-  }
+/**
+ * BAD:
+ * - 재사용되지 않고 한 번만 사용된다면 클래스로 만드는 것이 불필요하다.
+ *  - 예시) Math 유틸함수를 사용할 때 매번 new Math.max(1, 2) 하지 않고 static 메서드를 사용
+ *
+ * SOLUTION:
+ * - 일반 함수로 만든다.
+ *
+ * QUESTION:
+ * - Math 클래스처럼 static 메서드를 가지는 클래스로 만들어도 될까?
+ * */
+export function charge(customer, usage, provider) {
+  const baseCharge = customer.baseRate * usage;
+  return baseCharge + provider.connectionCharge;
 }
