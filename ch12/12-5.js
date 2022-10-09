846ad5ae3ca57bfd4645f5471a0b93570b45263e
Autumn <autumn@inflab.com>
2022-10-09 21:33:39 +0900

    [12-6] 필드 내리기

--- ch12/12-5.js
+++ ch12/12-5.js
@@ -1,6 +1,18 @@
-class Employee {
-  #quota;
-}
+/**
+ * BAD:
+ * - 불필요한 필드 상속
+ *
+ * SOLUTION:
+ * - 필요한 서브클래스에서만 필드를 정의한다.
+ *
+ * TIPS:
+ * - 설계를 먼저 해라.
+ * - extends, interface, abstract class, composition 중 어떤 것이 적절할지 생각하기
+ * */
+
+class Employee {}
 
 class Engineer extends Employee {}
-class Salesperson extends Employee {}
+class Salesperson extends Employee {
+  #quota;
+}
