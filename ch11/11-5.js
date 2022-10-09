beef86678bc7b6de6a9611cc1944a2459a310e9a
Autumn <autumn@inflab.com>
2022-10-09 13:57:06 +0900

    [11-5] 매개변수를 질의함수로 바꾸기

--- ch11/11-5.js
+++ ch11/11-5.js
@@ -1,23 +1,38 @@
+/**
+ * BAD:
+ * - 불필요하게 매개변수를 넘기고 있다.
+ *
+ * SOLUTION:
+ * - getter를 이용해 질의함수로 추출한다.
+ *
+ * TIPS:
+ * - 개선 포인트를 찾는 법
+ *  - 매개변수의 개수를 줄인다는 생각으로 접근하자.
+ * */
 export class Order {
   constructor(quantity, itemPrice) {
     this.quantity = quantity;
     this.itemPrice = itemPrice;
   }
 
-  get finalPrice() {
-    const basePrice = this.quantity * this.itemPrice;
-    let discountLevel;
-    if (this.quantity > 100) discountLevel = 2;
-    else discountLevel = 1;
-    return this.discountedPrice(basePrice, discountLevel);
+  get basePrice() {
+    return this.quantity * this.itemPrice;
   }
 
-  discountedPrice(basePrice, discountLevel) {
-    switch (discountLevel) {
+  get finalPrice() {
+    return this.discountedPrice();
+  }
+
+  get discountLevel() {
+    return this.quantity > 100 ? 2 : 1;
+  }
+
+  discountedPrice() {
+    switch (this.discountLevel) {
       case 1:
-        return basePrice * 0.95;
+        return this.basePrice * 0.95;
       case 2:
-        return basePrice * 0.9;
+        return this.basePrice * 0.9;
     }
   }
 }
