55b39291f68a88eab89db04a32474bcb3964719d
Autumn <autumn@inflab.com>
2022-10-09 21:25:01 +0900

    [12-5] 메서드 내리기

--- ch12/12-4.js
+++ ch12/12-4.js
@@ -1,6 +1,14 @@
-class Employee {
-  get quota() {}
-}
+/**
+ * BAD:
+ * - 불필요한 상속
+ *
+ * SOLUTION:
+ * - 필요한 자식식래스에만 메서드를 정의한다.
+ * */
+
+class Employee {}
 
 class Engineer extends Employee {}
-class Salesperson extends Employee {}
+class Salesperson extends Employee {
+  get quota() {}
+}
